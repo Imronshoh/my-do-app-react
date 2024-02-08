98be56271b92865ce0334a3fa13817e35ff41236
Imronshoh <monikapi2022@gmail.com>
2024-02-08 13:29:01 +0500

    created

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './App.css'
+
+let arr = [];
+
+function App() {
+  const [val, setVal] = useState('')
+  const [render, setRender] = useState(true)
+  function fnAdd() {
+    arr.push(val);
+    setRender(!render)
+  }
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="list">
+          <label class="input-group__label" for="myInput">My Label</label>
+          <input type="text" id="myInput" class="input-group__input" onChange={(e) => setVal(e.target.value)} />
+          <button class="button-29" role="button" onClick={fnAdd}>Button 29</button>
+
+
+        </div>
+      </div>
+      {
+        arr.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })
+      }
+    </div>
+  )
+}
+
+export default App
